b66e06427d24116f1e86f2f512a395e7feb5d94c
Ozodbek <zokirjonovozodbek9@gmail.com>
2026-01-22 12:21:05 +0500

    fixed 6

--- frontend_school_crm/src/pages/class-details.tsx
+++ frontend_school_crm/src/pages/class-details.tsx
@@ -119,7 +119,7 @@ export default function ClassDetailsPage() {
       return "paid";
     }
 
-    // If there's any payment but less than required, it's partial
+    // If there's any payment but less than required, it's partia
     if (paidTotal > 0) {
       return "partial";
     }
