24cc8414b10e1c4f9eaa19c4a726ef6732364bdc
Ozodbek <zokirjonovozodbek9@gmail.com>
2026-04-09 09:17:31 +0500

    fixed cors

--- backend_school_crm/internal/middleware/cors.go
+++ backend_school_crm/internal/middleware/cors.go
@@ -15,6 +15,9 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Branch-ID")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+		c.Writer.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+		c.Writer.Header().Set("Pragma", "no-cache")
+		c.Writer.Header().Set("Expires", "0")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
